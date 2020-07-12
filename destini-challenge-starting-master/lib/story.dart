16d664f485248c70ab0cf5a56ea57038d5ded84b
talhaSaqib <tsaqib8373@gmail.com>
2020-07-12 10:28:52 +0500

    destini

---
+++ destini-challenge-starting-master/lib/story.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({String storyTitle, String choice1, String choice2}) {
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+  }
+}
